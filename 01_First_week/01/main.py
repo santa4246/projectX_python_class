6e871bfbaa6c9cfc8784ac3b12a64233e0b5cca2
Minhyeok Kang <santa4246@naver.com>
2023-11-02 20:49:48 +0900

    디렉토리 구조 변경, 코드 리펙토링

---
+++ 01_First_week/01/main.py
@@ -0,0 +1,44 @@
+def main():
+    file_name = 'mission_computer_main.log'
+
+    # 수행과제
+    print_hello()
+    print_log(file_name)
+
+    # 보너스과제
+    print_log_reverse(file_name)
+    save_err_log(file_name)
+
+
+def print_hello():
+    print('Hello Mars')
+
+def print_log(file_name):
+    try:
+        with open(file_name) as infile :
+            for in_line in infile.readlines():
+                print(in_line)
+    except IOError as err:
+        print("I/O error: {0}".format(err))
+
+def print_log_reverse(file_name):
+    try:
+        with open(file_name) as infile :
+            for in_line in reversed(infile.readlines()):
+                print(in_line)
+    except IOError as err:
+        print("I/O error: {0}".format(err))
+
+def save_err_log(file_name):
+    try:
+        with open(file_name) as infile :
+            f = open("problem.txt", 'w')
+            for in_line in infile.readlines()[33:36]:
+                f.write(in_line)
+            f.close()
+    except IOError as err:
+        print("I/O error: {0}".format(err))
+
+
+if __name__ == "__main__":
+    main()
