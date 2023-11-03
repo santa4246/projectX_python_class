692e9c731efa967f0a8deb31a03e9bbcb407681b
Minhyeok Kang <santa4246@naver.com>
2023-11-04 00:44:48 +0900

    문제3 답안 작성, 코드 리팩토링

--- 01_First_week/02/main.py
+++ 01_First_week/02/main.py
@@ -1,22 +1,26 @@
+import json
+
 def main():
     file_name = 'mission_computer_main.log'
-
-    # 수행과제
-    print_log(file_name)
-    log_list = print_list(file_name)
-    list_to_dict(log_list)
-    reverse_list(log_list)
-    
-    # 보너스과제
+    try:
+        # 수행과제
+        print_log(file_name)
+        log_list = print_list(file_name)
+        reverse_list(log_list)
+        log_dict_list = list_to_dict(log_list)
+        save_dict_to_jsonfile(log_dict_list)
+        
+        # 보너스과제
+        search_text(log_dict_list)
+        
+    except IOError as err:
+        print("I/O error: {0}".format(err))
 
 
 def print_log(file_name):
-    try:
-        with open(file_name) as infile:
-            for in_line in infile.readlines():
-                print(in_line)
-    except IOError as err:
-        print("I/O error: {0}".format(err))
+    with open(file_name) as infile:
+        for in_line in infile.readlines():
+            print(in_line)
 
 def print_list(file_name):
     log_list = []
@@ -32,16 +36,31 @@ def list_to_dict(log_list):
     log_dict_list = []
     for log_item in log_list:
         log_dict = {
-            'key1': log_item[0],
-            'key2': log_item[1],
-            'key3': log_item[2]
+            'timestamp': log_item[0],
+            'event': log_item[1],
+            'message': log_item[2]
         }
         log_dict_list.append(log_dict)
 
     print(log_dict_list)
+    return log_dict_list
 
 def reverse_list(log_list):
     print(log_list[::-1])
+
+def save_dict_to_jsonfile(log_dict_list):
+    with open('mission_computer_main.json', 'w') as json_file:
+        json.dump(log_dict_list, json_file)
+
+    print("mission_computer_main.json 파일이 업데이트되었습니다.")
+
+def search_text(log_dict_list):
+    search_string = 'noticeable'
+
+    for log_dict in log_dict_list:
+        found_keys = [key for key, value in log_dict.items() if search_string in value]
+        if found_keys:
+            print(f"'{search_string}'을(를) 포함한 키: {found_keys}")
 
 if __name__ == "__main__":
     main()
